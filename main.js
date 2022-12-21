485816f52c293485d5345907240bf0f40b839338
darren-kk <rlawoghks10@gmail.com>
2022-12-22 07:06:35 +0900

    add flip image function

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const startBtn = document.querySelector(".btn_start");
 const footer = document.querySelector("footer");
 const remainTime = document.querySelector(".time");
+const remainChoi = document.querySelector(".choi");
 
 const mainImg = document.querySelector(".main_img_box");
 const loseImg = document.querySelector(".img_lose");
@@ -13,14 +14,17 @@ const clickedImgs = document.querySelectorAll(".clicked");
 
 const indexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-const newArray = [];
-let sec = 5;
+const randomImgsArray = [];
+let flippedArray = [];
+let unflippedArray = [];
+let sec = 25;
+let goal = 8;
 let intervalId = undefined;
 let count = 0;
 
 const handleClick = () => {
   handleStartBtn();
-  //startTimer();
+  startTimer();
   makeRandomImgs();
 }
 
@@ -37,7 +41,14 @@ const handleStartBtn = () => {
   else { // 재시작 버튼 클릭
     startBtn.textContent = "시작";
     mainImg.style.display = "block";
-    footer.style.display = "none"; 
+    footer.style.display = "none";
+    unflippedArray = [];
+    flippedArray = []; 
+    count = 0;
+    makeRandomImgs();
+    for (const el of unclickedImgs) {
+      el.style.visibility = "visible";
+    }
   }
 }
 
@@ -80,16 +91,50 @@ const makeRandomImgs = () => {
   const randomIndex = makeRandomNumbers(indexArray);
 
   for (let i = 0; i < clickedImgs.length; i++) {
-    newArray.push(clickedImgs[randomIndex[i]].src);
+    randomImgsArray.push(clickedImgs[randomIndex[i]].src);
   }
   
   for (let i = 0; i < clickedImgs.length; i++) {
-    clickedImgs[i].src = newArray[i];
+    clickedImgs[i].src = randomImgsArray[i];
   }
 }
 
+const handleUnclickedFlip = (event) => {
+  const unclickedImgsArray = Array.from(unclickedImgs);
+  count = count + 1;
+  event.target.style.visibility = "hidden";
 
+  if (count <= 2) {
+    flippedArray.push(randomImgsArray[unclickedImgsArray.indexOf(event.target)]);
+    unflippedArray.push(event.target)
+  }
+
+  if (count === 2) {
+    count = 0;
+
+    if (flippedArray[0] !== flippedArray[1]) {
+      setTimeout(() => {for (const el of unflippedArray) {
+        el.style.visibility = "visible";
+        unflippedArray = [];
+      }}, 300)
+      flippedArray = [];
+    } else {
+      unflippedArray[0].style.visibility = "hidden";
+      unflippedArray[1].style.visibility = "hidden";
+      unflippedArray = [];
+      flippedArray = [];
+      goal = goal - 1
+      remainChoi.textContent = goal + " 명";
+    }
+  }
+
+  console.log(unflippedArray);
+  console.log(flippedArray);
+}
 
 startBtn.addEventListener("click", handleClick);
 
+for (const el of unclickedImgs) {
+  el.addEventListener("click", handleUnclickedFlip);
+}
 
